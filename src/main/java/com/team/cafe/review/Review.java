fb1d45c7bb02bd2b3644eb7071f0074a358fc0c9
unknown <hjs54336297@gmail.com>
2025-08-22 16:22:42 +0900

    test

---
+++ src/main/java/com/team/cafe/review/Review.java
@@ -0,0 +1,11 @@
+package com.team.cafe.review;
+
+import jakarta.persistence.ManyToOne;
+import org.hibernate.annotations.Cascade;
+import org.springframework.context.annotation.Bean;
+
+public class Review {
+
+
+
+}
---
+++ src/main/java/com/team/cafe/review/ReviewService.java
@@ -0,0 +1,8 @@
+package com.team.cafe.review;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+}
+//안녕하셍쵸
