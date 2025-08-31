fff1517bf8543634314fddd398d7b43a3900f4e0
HwangJeongSeong <hjs54336297@gmail.com>
2025-08-31 21:13:32 +0900

    properties fix

--- src/main/java/com/team/cafe/repository/SiteUserRepository.java
+++ src/main/java/com/team/cafe/repository/SiteUserRepository.java
@@ -1,10 +1,18 @@
 package com.team.cafe.repository;
 
+import com.team.cafe.domain.Review;
 import com.team.cafe.domain.SiteUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface SiteUserRepository extends JpaRepository<SiteUser, Long> {
     Optional<SiteUser> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
