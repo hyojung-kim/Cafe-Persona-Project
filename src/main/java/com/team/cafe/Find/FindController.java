db8b817bc9e712de0143afe01829af9be2d073e9
0n0ng <hy991006@gmail.com>
2025-09-02 17:47:04 +0900

    findForm

--- src/main/java/com/team/cafe/Find/FindController.java
+++ src/main/java/com/team/cafe/Find/FindController.java
@@ -1,12 +1,29 @@
 package com.team.cafe.Find;
 
+import com.team.cafe.user.sjhy.SiteUser;
+import com.team.cafe.user.sjhy.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class FindController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/user/findId")
     public String findId() {
-        return "find_form";
+        return "/login/find_form";
     }
+
+//    @PostMapping("/user/findId")
+//    public String searchId(HttpServletRequest request, Model model,
+//                           @RequestParam String nickname,
+//                           @RequestParam String email) {
+//    }
 }
