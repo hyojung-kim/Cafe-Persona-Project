e97b428cbf757b42a1991cd2508b37cafc62d64a
0n0ng <hy991006@gmail.com>
2025-09-08 21:57:16 +0900

    bookmark page 만

---
+++ src/main/java/com/team/cafe/bookmark/BookmarkController.java
@@ -0,0 +1,15 @@
+package com.team.cafe.bookmark;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class BookmarkController {
+
+    @GetMapping("/mypage/my/bookmark")
+    public String myBookmark() {
+        return "mypage/my_bookmark";
+    }
+}
