4d1aa62b7f3817e8df354287f73771f0ba994701
hyo <rlagywnd05@gmail.com>
2025-09-22 00:03:37 +0900

    카페메인 준비작업

--- src/main/java/com/team/cafe/Root/MainRepository.java
+++ src/main/java/com/team/cafe/Root/MainRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MainRepository extends JpaRepository<Cafe, Long> {
-    List<Cafe> findTop8ByOrderByHitCountDesc();
+    List<Cafe> findTop4ByOrderByHitCountDesc();
 }
